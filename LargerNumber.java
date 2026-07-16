d6230c4064a93fe0515932e2708e3032033b871a
Hariharan24-Ai <earlharry6@gmail.com>
2026-07-16 14:09:05 +0530

    new

--- LargerNumber.java
+++ LargerNumber.java
@@ -8,6 +8,8 @@ public class LargerNumber {
             System.out.println(num1 + " is the larger number.");
         } else if (num2 > num1) {
             System.out.println(num2 + " is the larger number.");
+        } else if (num1 < num2) {
+            System.out.println(num1 + " is the smaller number.");
         } else {
             System.out.println("Both numbers are equal.");
         }
