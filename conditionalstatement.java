926be866d219e8479c8e3d0fd76f302bf3e1b518
Hariharan24-Ai <earlharry6@gmail.com>
2026-07-16 13:25:07 +0530

    conditonalstatement using vote age

---
+++ conditionalstatement.java
@@ -0,0 +1,11 @@
+public class conditionalstatement {
+    public static void main(String[] args) {
+        int age = 10;
+
+        if (age > 18) {
+            System.out.println("Your are above 18.");
+        } else {
+            System.out.println("Your are not above 18.");
+        }
+    }
+}
