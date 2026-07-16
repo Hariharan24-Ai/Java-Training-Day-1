161f835c079c69177a9b2d099fd2224aeafba2f9
Hariharan24-Ai <earlharry6@gmail.com>
2026-07-16 14:00:25 +0530

    new

---
+++ OddEven.java
@@ -0,0 +1,12 @@
+public class OddEven {
+    public static void main(String[] args) {
+
+        int number = 8;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
