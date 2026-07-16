afcf202ad319f67532e99eb02c21222e4f642ee5
Hariharan24-Ai <earlharry6@gmail.com>
2026-07-16 15:57:08 +0530

    new

---
+++ ArrayWhileLoop.java
@@ -0,0 +1,13 @@
+public class ArrayWhileLoop {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        int i = 0;
+
+        while (i < numbers.length) {
+            System.out.println(numbers[i]);
+            i++;
+        }
+    }
+}
