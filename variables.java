5d248f56ec06ddb09e832cf624fabc5e483d2222
Hariharan24-Ai <earlharry6@gmail.com>
2026-07-16 12:08:16 +0530

    Initial commit

---
+++ variables.java
@@ -0,0 +1,28 @@
+public class variables {
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 5;
+    String i = "hari";
+    String name = "haran";
+    byte z= 100;
+    short s = 20000;
+    int p= 500000;
+    long l = 9000000000L;
+    float f = 5.75f;
+    double d = 19.99;
+    char c = 'J';
+    boolean flag = true;
+    int [] arr = {1, 2, 3, 4, 5};
+    System.out.println("byte: " + z);
+    System.out.println("short: " + s);
+    System.out.println("int: " + p);
+    System.out.println("long: " + l);
+    System.out.println("float: " + f);
+    System.out.println("double: " + d);
+    System.out.println("char: " + c);
+    System.out.println("boolean: " + flag);
+    System.out.println((a + b)+" "+i+" "+name);
+    System.out.println("Array elements: " + arr[1]);
+    
+    }
+}
